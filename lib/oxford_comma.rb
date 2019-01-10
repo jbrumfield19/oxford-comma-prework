283487d8e564eac49325e776aa1cf27eedf4c024
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-10 00:19:31 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,14 @@
 def oxford_comma(array)
- array.join(" and ")
- 
+number_of_words = array.size
+
+
+ case number_of_words
+    when 1
+      array[0]
+    when 2
+      array.join(" and ")
+    else
+      last_word = array.pop
+      array.join(", ") << ", and #{last_word}"
+  end
 end
